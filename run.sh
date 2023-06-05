a97a8801ec8bfce9ddd8a89921711a75041e2f56
root <root@instance-20230530-2015.subnet05302019.vcn05302019.oraclevcn.com>
2023-06-05 10:19:42 +0000

    Saved - 2023-06-05 10:19:42

--- run.sh
+++ run.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env sh
-# Forge requires a configured set of both JVM and program arguments.
-# Add custom JVM arguments to the user_jvm_args.txt
-# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
-#  pass them to this script directly
+
+# Realiza un git pull para obtener los últimos cambios
+git pull
+
+# Añade todos los cambios al área de preparación de Git
+git add .
+
+# Guarda la fecha y hora actuales en una variable
+currentDateTime=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Realiza un commit con estos cambios
+git commit -m "Saved - $currentDateTime"
+
+# Sube estos cambios a GitHub
+git push origin master
+
+# Inicia el servidor
 java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.4-45.0.66/unix_args.txt --nogui "$@"
+
